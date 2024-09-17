bf0fe20eda7da2eb1372feaea624488ed622ef96
Arthur Lyra Miranda <82905393+arthur-lyra@users.noreply.github.com>
2024-09-16 18:04:03 -0900

    Add files via upload

---
+++ Decorator/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        String salaryRecords = "Name,Salary\n Arthur Lyra,100000\nEmmy Lyra,912000";
+        DataSourceDecorator encoded = new CompressionDecorator(
+                new EncryptionDecorator(
+                        new FileDataSource("out/OutputDemo.txt")));
+        encoded.writeData(salaryRecords);
+        DataSource plain = new FileDataSource("out/OutputDemo.txt");
+
+        System.out.println("- Input ----------------");
+        System.out.println(salaryRecords);
+        System.out.println("- Encoded --------------");
+        System.out.println(plain.readData());
+        System.out.println("- Decoded --------------");
+        System.out.println(encoded.readData());
+    }
+}
